f553c1203284dfd208f1b51addec08474d9b7d95
Carlos Eduardo <carlostecnicowl@gmail.com>
2025-01-16 18:32:21 -0300

    DOC: Adiciona documentação para funções de formatação e manipulação de tabelas no simulador

--- js/simulador.js
+++ js/simulador.js
@@ -1,16 +1,49 @@
-
+/**
+ * Formata um valor dado como uma string de moeda em Real Brasileiro (BRL).
+ *
+ * @param {number|string} valor - O valor a ser formatado. Pode ser um número ou uma string.
+ * @returns {string} - A string de moeda formatada no local 'pt-BR'. 
+ * Se a entrada não for um número, retorna a entrada como está.
+ */
 function formatarValor(valor) {
     if (isNaN(valor)) {
         return valor;
     }
-    return Number.parseFloat(valor).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+    return Number.parseFloat(valor)
+    .toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
 }
+
+
+/**
+ * Converte o valor de um elemento de entrada HTML para um número de ponto flutuante.
+ *
+ * @param {HTMLInputElement} valor - O elemento de entrada HTML cujo valor será convertido.
+ * @returns {number} A representação numérica de ponto flutuante do valor de entrada.
+ */
 function conveterHTMltoFloat(valor) {
     return Number.parseFloat(valor.value);
 }
+
+/**
+ * Converte o valor de um elemento de entrada HTML para um número inteiro.
+ *
+ * @param {HTMLInputElement} valor - O elemento de entrada HTML cujo valor será convertido.
+ * @returns {number} A representação numérica inteira do valor de entrada.
+ */
 function conveterHTMltoInt(valor) {
     return Number.parseInt(valor.value);
 }
+
+/**
+ * Alimenta a tabela HTML com os valores fornecidos para mês, saldo, amortização, juros e prestação.
+ *
+ * @param {number|string} mes - O mês atual ou uma string representando o mês.
+ * @param {number} saldo - O saldo atual.
+ * @param {number} amortizacao - O valor da amortização.
+ * @param {number} juros - O valor dos juros.
+ * @param {number} prestacao - O valor da prestação.
+ * @returns {string} - A linha de tabela HTML gerada.
+ */
 function alimentarTabela(mes, saldo, amortizacao, juros, prestacao) {
     let carencia = conveterHTMltoInt(document.getElementById('numeroCarencia'));
     let texto = '<tr>' +
@@ -22,10 +55,28 @@ function alimentarTabela(mes, saldo, amortizacao, juros, prestacao) {
         '</tr>';
     return texto;
 }
+
+/**
+ * Gera o cabeçalho da tabela HTML com o nome fornecido.
+ *
+ * @param {string} nome - O nome a ser exibido no cabeçalho da tabela.
+ * @returns {string} - O cabeçalho da tabela HTML gerado.
+ */
 function cabelho(nome) {
     return '<table border="1" class = "simulacao" >' + '<tr><td colspan ="5">' + nome + '</td></tr>'
         + alimentarTabela('Mês', 'Saldo', 'Amortização', 'Juros', 'Prestação');
 }
+
+
+/**
+ * Generates a table row with the total amortization, total interest, total payment, and interest rate.
+ *
+ * @param {number} totalAmortizacao - The total amortization amount.
+ * @param {number} totalJuros - The total interest amount.
+ * @param {number} totalPrestacao - The total payment amount.
+ * @param {number} taxaJuros - The interest rate per month.
+ * @returns {string} The HTML string representing the table row with the totals and interest rate.
+ */
 function linhaTotal(totalAmortizacao, totalJuros, totalPrestacao, taxaJuros) {
     return '<tr><th colspan = "2">Total</th><th>'
         + formatarValor(totalAmortizacao) + '</th><th>'
@@ -36,6 +87,13 @@ function linhaTotal(totalAmortizacao, totalJuros, totalPrestacao, taxaJuros) {
         + '</table>';
 }
 
+/**
+ * Gera tabelas de amortização de empréstimos com base nos valores de entrada do usuário.
+ * Recupera valores de elementos HTML, valida-os e, em seguida, gera
+ * tabelas usando diferentes métodos de amortização (SAC, SPC, SAM).
+ *
+ * @function tabela
+ */
 function tabela() {
     let valorEmprestimoHTML = conveterHTMltoFloat(document.getElementById('valorEmprestimo'));
     let taxaJurosHTML = conveterHTMltoFloat(document.getElementById('taxaJuros'));
@@ -51,12 +109,27 @@ function tabela() {
 }
 
 
+/**
+ * Formata uma taxa de imposto dada como uma string de porcentagem.
+ *
+ * @param {number|string} taxa - A taxa de imposto a ser formatada. Pode ser um número ou uma string.
+ * @returns {string} A taxa de imposto formatada como uma string de porcentagem com quatro casas decimais.
+ */
 function formatarTaxa(taxa) {
     if (isNaN(taxa)) {
         return taxa;
     }
     return Number.parseFloat(taxa/100).toLocaleString('pt-BR', { style: 'percent', minimumFractionDigits: 4 });
 }
+
+/**
+ * Determina a classe CSS a ser aplicada com base no mês e no número de períodos de carência.
+ *
+ * @param {number|string} mes - O mês atual ou uma string representando o mês.
+ * @param {number} numeroCarencia - O número de períodos de carência.
+ * @returns {string} A classe CSS a ser aplicada. 
+ * Retorna uma string vazia se o mês for 0 ou "Mês".
+ */
 function ecarencia(mes, numeroCarencia) {
     if (mes === 0 || mes=== "Mês") {
         return "";
