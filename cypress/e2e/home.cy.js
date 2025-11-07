6bf64ce6176decc2cdb5bbe85a46ab616ae08368
CarlosQA <carlostecnicowl@gmail.com>
2025-11-07 17:13:10 -0300

    Adiciona validação para o campo 'numeroCarencia' e seleciona o tipo de carência no teste de simulação de empréstimo

--- cypress/e2e/home.cy.js
+++ cypress/e2e/home.cy.js
@@ -4,6 +4,12 @@ describe('template spec', () => {
     cy.get('#valorEmprestimo').clear().type('5000');
     cy.get('#taxaJuros').clear().type('12');
     cy.get('#numeroCarencia').clear().type('24');
+    cy.get('#numeroCarencia').invoke('text').then((texto) => {
+      const numero = parseFloat(texto);
+      expect(numero).to.be.greaterThan(0);
+    });
+    cy.get('#tipoCarencia').select('Juro Capitalização e Acrescido de saldo Devedor');
+  
     cy.get('#numeroPrestacao').clear().type('36');
   });
 });
