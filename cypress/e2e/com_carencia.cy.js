0100ab367fc4f312ac9464d981c73297dd237e5d
CarlosQA <carlostecnicowl@gmail.com>
2025-11-08 00:07:29 -0300

    Adiciona testes de simulação de empréstimo com e sem carência no Cypress

---
+++ cypress/e2e/com_carencia.cy.js
@@ -0,0 +1,48 @@
+describe('Simulador de Empréstimo Com Carência ', () => {
+  
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    cy.visit('https://simulador-de-emprestimo-two.vercel.app/');
+    cy.get('#valorEmprestimo').clear().type('5000');
+    cy.get('#taxaJuros').clear().type('12');
+    cy.get('#numeroCarencia').trigger('mouseover').clear().type('24').trigger('mouseout');
+  });
+
+  afterEach(() => {
+    cy.get('#numeroPrestacao').clear().type('36');
+    cy.get('#btnSimular').click();
+  });
+
+  it('com Pagamento de Juros', () => {
+    cy.get('#numeroCarencia').invoke('val').then((texto) => {
+      const numero = parseFloat(texto);
+      if (numero > 0) {
+         cy.get('#tipoCarencia').select('PJ', { force: true }).wait(1000);
+      }
+    });
+    //cy.wait(10000).screenshot();
+  });
+  
+  it('com Capitalização de Juros', () => {
+    cy.get('#numeroCarencia').invoke('val').then((texto) => {
+      const numero = parseFloat(texto);
+      if (numero > 0) {
+         cy.get('#tipoCarencia').select('CJ', { force: true }).wait(1000);
+      }
+    });
+
+    //cy.wait(10000).screenshot();
+  });
+  
+  it('com Juro Capitalização e Acrescido de Saldo Devedor', () => {
+    cy.get('#numeroCarencia').invoke('val').then((texto) => {
+      const numero = parseFloat(texto);
+      if (numero > 0) {
+         cy.get('#tipoCarencia').select('JCASD', { force: true }).wait(1000);
+      }
+    });
+    //cy.wait(10000).screenshot();
+  });
+
+});
+
---
+++ cypress/e2e/sem_carencia.cy.js
@@ -0,0 +1,25 @@
+describe('Simulador de Empréstimo Sem Carência ', () => {
+  
+  beforeEach(() => {
+    cy.viewport(1280, 720);
+    cy.visit('https://simulador-de-emprestimo-two.vercel.app/');
+    cy.get('#valorEmprestimo').clear().type('5000');
+    cy.get('#taxaJuros').clear().type('12');
+    cy.get('#numeroCarencia').trigger('mouseover').clear().type('0').trigger('mouseout');
+  });
+
+  afterEach(() => {
+    cy.get('#numeroPrestacao').clear().type('36');
+    cy.get('#btnSimular').click();
+  });
+
+  it('Simples', () => {
+    
+  });
+  
+
+  
+ 
+
+});
+
